e25064cac7ac182aaa0a2603533ea07c19704662
kenzi <kenzi@7f8d5f90-ea45-d24d-a860-8c9e05671eaf>
2021-07-06 02:04:05 +0000

    删除讯息与对应档案
    git-svn-id: https://172.18.0.240:8443/im/branches/server@7 7f8d5f90-ea45-d24d-a860-8c9e05671eaf

--- controllers/delete.js
+++ controllers/delete.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: Kenzi
  * @Date: 2021-06-16 10:28:22
- * @LastEditTime: 2021-07-02 19:01:39
+ * @LastEditTime: 2021-07-06 18:04:14
  * @LastEditors: Kenzi
  */
 import ChatRoomModel from "../models/ChatRoom.js";
@@ -41,61 +41,50 @@ export default {
           },
         },
       }));
-      if (!validation.success) return res.status(400).json({ ...validation });
+      
+      if (!validation.success) return res.status(400).json({ error: validation.error });
       let withFilesIds = [];
 
       const findMessages = await ChatMessageModel.findMessage(message_ids);
 
       findMessages.forEach((msg) => {
-        console.log("findMessages :>> ", findMessages);
         const files = msg.file;
-        console.log("files :>> ", files);
         files.forEach((file) => {
           withFilesIds.push(file._id);
         });
       });
+      
 
-      const findFile = await gfs.deleteMany({ _id: { $in: withFilesIds } });
+      if(withFilesIds.length > 0){
+     withFilesIds.forEach(  async file => {
+        const deleteFile = await gfs.delete(file)
+        })
+      }
       const deleteAllMessages = await ChatMessageModel.deleteMany({
         _id: { $in: message_ids },
       });
-      console.log("findFile :>> ", findFile);
-      // console.log("findMessages :>> ", findMessages);
-      // console.log("deleteAllMessages :>> ", deleteAllMessages);
-      // const deleteMessages = await message_ids.forEach(
-      //   async (message_id, index) => {
-      //     const messageInfo = await ChatMessageModel.find();
+     
+      
 
-      //     if (messageInfo[0]) {
-      //       const withFiles = messageInfo[0].file;
-      //       withFiles.forEach(async (item) => {
-      //         const findFile = await gfs.delete(item._id);
-      //       });
-      //       const message = await ChatMessageModel.deleteOne({
-      //         _id: message_id,
-      //       });
-      //     }
-      //   }
-      // );
+      const { user_ids } = await ChatRoomModel.getChatRoomUsersByRoomId(
+        room_id
+      );
 
-      // const { user_ids } = await ChatRoomModel.getChatRoomUsersByRoomId(
-      //   room_id
-      // );
-
-      // if (user_ids) {
-      //   console.log("user_ids :>> ", user_ids);
-      //   await emitUsersExceptSender(
-      //     currentLoginSocketId,
-      //     user_ids,
-      //     "delete_message",
-      //     {
-      //       message_ids: message_ids,
-      //     }
-      //   );
-      // }
+      if (user_ids) {
+         emitUsersExceptSender(
+          currentLoginSocketId,
+          user_ids,
+          "delete_message",
+          {
+            chat_room_id: room_id,
+            message_ids: message_ids,
+          }
+        );
+      }
 
       return res.status(200).json({
         success: true,
+        ...deleteAllMessages
       });
     } catch (error) {
       console.log("error :>> ", error);
