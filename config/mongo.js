e88ec86b9c2e1ed84e84dfd4c2ceedb9fbc5c655
kenzi <kenzi@7f8d5f90-ea45-d24d-a860-8c9e05671eaf>
2021-08-08 17:39:22 +0000

    change mongo config
    git-svn-id: https://172.18.0.240:8443/im/branches/server@47 7f8d5f90-ea45-d24d-a860-8c9e05671eaf

--- config/mongo.js
+++ config/mongo.js
@@ -2,14 +2,14 @@
  * @Description:
  * @Author: Kenzi
  * @Date: 2021-06-10 18:32:02
- * @LastEditTime: 2021-08-02 15:11:48
+ * @LastEditTime: 2021-08-09 09:37:55
  * @LastEditors: Kenzi
  */
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 const config = dotenv.config();
 
-export const MONGO_URL = process.env.MANGO_DB_URL_DEVELOPMENT;
+export const MONGO_URL = process.env.MANGO_DB_URL_PRODUCTION;
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
